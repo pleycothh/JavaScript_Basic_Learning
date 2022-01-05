624b59f3706e146b5b302cb49849f68982db9305
Ben Li <ben.li19930119@gmail.com>
2022-01-05 18:54:42 +1000

    ss

--- Js/index.js
+++ Js/index.js
@@ -1,4 +1,117 @@
 /// ##########  Objects ###########
 
+let circle = {
+    radius: 1,    
+    location: {
+        x:1,
+        y:2
+    },
+    isVisiable: true,
+    draw: function() {
+        console.log('draw');
+    } // method if in an object
+};
+//circle.draw();\
+
+/// Factory function
+
+function createCircle(radiu, x, y, isVisiable){
+    return {
+        radiu,    
+        location: {
+            x,
+            y
+        },
+        isVisiable,
+        draw(){
+            console.log('draw',x,y,radiu);
+        }
+    }
+};
+
+circleOne = createCircle(10,1,1,true);
+circleTwo = createCircle(1,1,1,false);
+
+//circleOne.draw();
+//circleTwo.draw();
+
+/// Constructor Functions
+
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log("draw");
+    }
+    // return this
+}
+
+// see this is an empty object
+
+const circleThree = new Circle(1); // create new empty object
+
+// create empty object called this
+
+// give x an value
+// return this
+
+/// Dynamic nature of object
+
+circleThree.color = 'yellow';
+circleThree.draaa = function(){};
+delete circleThree.color;
+//console.log(circleThree);
+
+///Constructor property
+
+console.log(circle.constructor)
+
+let x = {};
+// let x = new Object();
+
+/// function are objects
+
+Circle.call({}, 1);
+const another = new Circle(1);
+
+/// Value type and Reference Type
+ 
+let number = {value:10};
+
+function increase(number){
+    number.value++
+}
+
+increase(number);
+console.log(number)
 
 
+/// Enumerating property object
+
+for (let key in circle){
+    console.log(key, circle[key]);
+}
+
+for (let key of Object.keys(circle))
+    console.log(key)
+
+/// colon
+
+const another2 = Object.assign({color:"yellow"}, circle);
+const another3 = {...circle};
+console.log(another3)
+
+// Garbage collection
+//no control
+
+///math
+
+//developer.mozilla.org search math 
+
+/// string
+
+// string primate
+const message = 'hi';
+
+
+//string object
+const anotherMessage = new String('jo');
