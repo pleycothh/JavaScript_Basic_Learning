3332dcd835c71295b580c542e0b29b65079f6297
Ben Li <ben.li19930119@gmail.com>
2022-01-06 22:46:44 +1000

    add rest operator

--- Js/Functions.js
+++ Js/Functions.js
@@ -1,1 +1,23 @@
 console.log('function start here')
+
+// function are objects
+
+/// 
+
+function sum (){
+    let total = 0;
+    for(let value of arguments)
+        total += value;
+    return total;
+}
+console.log(sum(1,2,3,4,5,6))
+
+/// REST operator
+
+
+function sum2 (discount, ...prices){ // rest operators, rest of the parameters
+    const total = prices.reduce((a,b) => a+b);
+    return total * (1- discount)
+
+}
+console.log(sum2(0.1,2,3,4,5,6,1))
