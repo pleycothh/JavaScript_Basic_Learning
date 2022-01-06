7086f1353de1989bac917cad78db76c851b26c15
tia <ben.li@griffithuni.edu.au>
2022-01-07 02:11:58 +1000

    functions

--- Js/Functions.js
+++ Js/Functions.js
@@ -21,3 +21,22 @@ function sum2 (discount, ...prices){ // rest operators, rest of the parameters
 
 }
 console.log(sum2(0.1,2,3,4,5,6,1))
+
+// getter and setter
+
+const person = {
+    firstName: "ben",
+    lastName: "Li",
+
+    get fullName(){
+        return `${this.firstName} ${this.lastName}`
+    },
+    set fullName(value){
+        const parts = value.split(' ');
+        this.firstName = parts[0];
+        this.lastName = parts[1]
+    }
+};
+
+person.fullName = 'John Liu'
+console.log(person.fullName)
